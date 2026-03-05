69f007a274f876859b3e50265d18d6005cefe069
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 15:14:45 +0200

    Tweaked DefaultController type to accept optional next function

--- ecommerce-backend/src/types/express/controller.d.ts
+++ ecommerce-backend/src/types/express/controller.d.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-type DefaultController = (req: Request, res: Response) => Promise<void>;
+type DefaultController = (req: Request, res: Response, next?: NextFunction) => Promise<void>;
 
 export default DefaultController;
