760f4e8402ea766327921dd7b49c8fc839f9c4fb
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 08:21:12 +0200

    Setup index.ts

---
+++ ecommerce-backend/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import { connect } from 'node:http2';
+import console from 'node:console';
+
+type StartFunction = () => Promise<void>;
+
+dotenv.config({
+  quiet: true
+});
+
+const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+
+const port: string = process.env.PORT || '3000';
+
+const start: StartFunction = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, (): void => console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    
+  }
+}
+
+start();
