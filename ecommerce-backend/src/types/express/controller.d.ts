f8daf2782f12986ec0a5c84130af9f2f79f582f8
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 15:21:41 +0200

    Tweaked DefaultController type to accept optional userId on request interface

--- ecommerce-backend/src/types/express/controller.d.ts
+++ ecommerce-backend/src/types/express/controller.d.ts
@@ -1,5 +1,15 @@
-import { NextFunction, Request, Response } from "express";
+import * as express from "express";
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
 
 type DefaultController = (req: Request, res: Response, next?: NextFunction) => Promise<void>;
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: mongoose.Types.ObjectId
+    }
+  }
+}
 
 export default DefaultController;
