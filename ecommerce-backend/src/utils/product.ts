7e63b7a8e5cea45dc015c2dac1208edb7e31eae2
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 13:48:17 +0200

    Added select function logic

--- ecommerce-backend/src/utils/product.ts
+++ ecommerce-backend/src/utils/product.ts
@@ -1,23 +1,53 @@
+import { BadRequestError } from "../errors";
+
 type QueryObject = Record<string, 1 | -1>;
-type FormatFunction = (sort: string) => QueryObject;
+type FormatFunction = (query: string) => QueryObject;
 
 export const formatSort: FormatFunction = (sort) => {
   const sortObj: QueryObject = {};
+  
   if (sort) {
     const entries = sort.split(',');
-    for (const entry of entries) {
+    
+    entries.forEach(entry => {
+      
       if (entry.startsWith('-')) {
         sortObj[entry.slice(1)] = -1;
       } else {
         sortObj[entry] = 1;
       }
-    }
+
+    });
   }
+
   return sortObj;
 }
 
 export const formatSelect: FormatFunction = (select) => {
   const selectObj: QueryObject = {};
+  
+  if (select) {
+    const entries = select.split(',');
+    
+    if (entries.every(entry => entry.startsWith('-')) || entries.every(entry => !entry.startsWith('-'))) {
+      entries.forEach(entry => {
+        entry.startsWith('-') ? selectObj[entry.slice(1)] = -1 : selectObj[entry] = 1;
+      });
+    } else {
+      const excludeEntries = entries.filter(entry => {
+        if (entry.startsWith('-')) {
+          return entry;
+        }
+      });
+      
+      if (excludeEntries.length > 1 || excludeEntries[0] !== "-_id") throw new BadRequestError("Exclude and include parameters may only join exclude _id only");
+      
+      entries.forEach(entry => {
+        entry.startsWith('-') ? selectObj[entry.slice(1)] = -1 : selectObj[entry] = 1;
+      });
+    }
 
+  }
+  
   return selectObj;
 }
