ab37d13c2a70baa8829259531aa07357443aac3b
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 15:45:55 +0200

    Implement authorize middleware

---
+++ ecommerce-backend/src/middleware/authorize.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { DefaultController } from '../types/express';
+import { UnAuthenticatedError } from '../errors';
+
+export const authorizeUser: DefaultController = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) return next!(new UnAuthenticatedError("Not authorized to access this route"));
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.userId = payload as Express.Request["userId"];
+    next!();
+  } catch (error) {
+    next!(new UnAuthenticatedError("Not authorized to access this route"));
+  }
+
+}
