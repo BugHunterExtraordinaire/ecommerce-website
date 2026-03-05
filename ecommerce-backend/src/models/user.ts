1554fa274ae1fffcf27577e2ccba6889942ec6d0
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-05 11:18:11 +0200

    Added user interface and exported model (COMPLETE)

--- ecommerce-backend/src/models/user.ts
+++ ecommerce-backend/src/models/user.ts
@@ -1,5 +1,12 @@
 import { genSalt, hash, compare } from 'bcryptjs';
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  verifyPassword(password: string): Promise<boolean>;
+}
 
 const userSchema = new Schema({
   name: {
@@ -24,8 +31,10 @@ const userSchema = new Schema({
 userSchema.pre('save', async function() {
   const salt = await genSalt(14);
   this.password = await hash(this.password, salt);
-})
+});
 
 userSchema.method('verifyPassword', async function(password) {
   return await compare(password, this.password);
-})
+});
+
+export default model<IUser>('User', userSchema);
