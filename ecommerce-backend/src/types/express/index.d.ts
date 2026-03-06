de66c2bec522619b2597b6b445dc3b4f004f4086
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-06 09:23:36 +0200

    Added new optional field to express request

--- ecommerce-backend/src/types/express/index.d.ts
+++ ecommerce-backend/src/types/express/index.d.ts
@@ -1,10 +1,16 @@
+import mongoose from 'mongoose';
 import * as express from "express";
 import { default as DefaultController } from './controller';
 
 declare global {
   namespace Express {
     interface Request {
-      userId?: mongoose.Types.ObjectId
+      userId?: mongoose.Types.ObjectId,
+      product?: {
+        productId: mongoose.Types.ObjectId,
+        price: number,
+        quantity: number,
+      }
     }
   }
 }
