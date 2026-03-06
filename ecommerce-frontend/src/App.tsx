e8af98684a33e0d41289aca039cc12dc652b384b
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-06 14:54:08 +0200

    Finalize pages and initial setup (COMPLETE)

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,10 +1,46 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import Checkout from './pages/Checkout';
+import ProductPage from './pages/ProductPage';
+import Error from './pages/Error';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return (
-    <>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/register",
+          element: <Register />
+        },
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "/product",
+          element: <ProductPage />
+        },
+        {
+          path: "/checkout",
+          element: <Checkout />
+        }
+      ]
+    }
+  ]);
 
-    </>
+  return (
+    <RouterProvider router={router} />
   )
 }
 
--- ecommerce-frontend/vite.config.ts
+++ ecommerce-frontend/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 5000, 
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
