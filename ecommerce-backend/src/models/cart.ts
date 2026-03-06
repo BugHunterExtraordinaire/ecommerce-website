b86f4b8f83b5dd1e4441681ea6b81ea5bcc6360c
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-06 08:27:50 +0200

    Added cart model with auto calculate total functionality (COMPLETE)

---
+++ ecommerce-backend/src/models/cart.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model } from "mongoose";
+import { ICart } from '../types/mongoose/interfaces/models';
+
+const cartSchema: Schema = new Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    require: true,
+    unique: true,
+    ref: "User",
+  },
+  products: [
+    {
+      productId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Product",
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+      price: Number,
+      
+    },
+  ],
+  total: Number
+});
+
+cartSchema.pre('save', function(this: ICart) {
+  this.total = this.products.reduce((prev, current) => prev += current.price, 0);
+});
+
+export default model<ICart>("Cart", cartSchema);
