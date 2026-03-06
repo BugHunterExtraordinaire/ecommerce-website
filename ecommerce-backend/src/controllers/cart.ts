f16bf8a0480c0d05b320d9d81a01e52812b3f54f
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-06 09:14:20 +0200

    Added createUserCart controller

--- ecommerce-backend/src/controllers/cart.ts
+++ ecommerce-backend/src/controllers/cart.ts
@@ -1,8 +1,26 @@
+import { StatusCodes } from 'http-status-codes';
 import { default as Cart } from '../models/cart';
+import { default as Product } from '../models/product';
 import { DefaultController } from "../types/express/controller";
+import { BadRequestError, NotFoundError } from '../errors';
 
 const createUserCart: DefaultController = async (req, res) => {
+  const { quantity } = req.body;
+  const product = await Product.findById(req.params.productId).select("_id price stock");
 
+  if (!product || product.stock === 0) throw new NotFoundError("Product out of stock");
+  if (product.stock < Number(quantity)) throw new BadRequestError("Not enough in stock");
+
+  const cart = await Cart.create({
+    userId: req.userId,
+    products: [{
+      productId: product._id,
+      quantity: Number(quantity),
+      price: product.price,
+    }]
+  });
+  
+  res.status(StatusCodes.CREATED).json(cart);
 }
 
 const getUserCart: DefaultController = async (req, res) => {
