eb250356059f231803587754bea8280c743da23b
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-06 09:50:22 +0200

    Implement getUserCart controller

--- ecommerce-backend/src/controllers/cart.ts
+++ ecommerce-backend/src/controllers/cart.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import { default as Cart } from '../models/cart';
 import { DefaultController } from "../types/express/controller";
+import { NotFoundError } from '../errors';
 
 const createUserCart: DefaultController = async (req, res) => {
   const { userId, product } = req;
@@ -18,7 +19,12 @@ const createUserCart: DefaultController = async (req, res) => {
 }
 
 const getUserCart: DefaultController = async (req, res) => {
+  const cart = await Cart.findOne({
+    userId: req.userId
+  });
+  if (!cart) throw new NotFoundError(`No cart was found for user: ${req.userId}`);
 
+  res.status(StatusCodes.OK).json(cart);
 }
 
 const updateUserCart: DefaultController = async (req, res) => {
