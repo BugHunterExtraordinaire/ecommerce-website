75c0ea2536942abcfe3d13e5ad1eca70350cc6e2
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-08 14:18:28 +0200

    Implement credentials heading

--- ecommerce-frontend/src/components/layout/Header.tsx
+++ ecommerce-frontend/src/components/layout/Header.tsx
@@ -1,6 +1,12 @@
+import { useLocation } from "react-router-dom";
+import { default as CredentialsHeaders } from './header-components/CredentialsHeaders.tsx';
+
 export default function Header() {
+  const location = useLocation();
+
   return (
     <>
+      {(location.pathname === '/login' || location.pathname === '/register') && <CredentialsHeaders />}
     </>
   );
 }
---
+++ ecommerce-frontend/src/components/layout/header-components/CredentialsHeaders.tsx
@@ -0,0 +1,7 @@
+export default function CredentialsHeaders() {
+  return (
+    <header className="flex justify-center items-center w-full h-15 text-(--font-color) bg-(--bg-color)">
+      <h1 className="text-3xl font-bold">Welcome to ProShop</h1>
+    </header>
+  );
+}
