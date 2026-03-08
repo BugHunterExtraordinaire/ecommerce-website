2f72f41037c4123a4f41f58df0a7f90452a65a50
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-08 13:42:43 +0200

    Intialized Header and Footer components for layout

--- ecommerce-frontend/src/pages/Layout.tsx
+++ ecommerce-frontend/src/pages/Layout.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { default as Header } from '../components/layout/Header'
+import { default as Footer } from '../components/layout/Footer'
+
 export default function Layout() {
   return (
     <>
-
+      <Header />
+      <Outlet />
+      <Footer />      
     </>
   );
 }
